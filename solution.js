6ec893d0fba40afe37c92e94c514b21d539fa6b8
xaoccc <xaocccc@gmail.com>
2024-09-26 10:57:49 +0300

    js update

--- solution.js
+++ solution.js
@@ -1,5 +1,4 @@
-let reservation =
-{
+let reservation = {
     startDate: null,
     endDate: null,
     guestsCount: 0,
@@ -7,17 +6,47 @@ let reservation =
     name: null,
     phone: null,
     email: null
-}
+};
+const searchContent = document.querySelector('.search-form-content');
+const thankYouContent = document.querySelector('.thank-you-content');
+const searchFormButton = document.querySelector('#search-form-button');
+const newReservationButton = document.querySelector('#new-reservation');
 
-function changeContent(className) {
-    document.querySelectorAll('.custom-form').forEach(div => div.classList.add('hidden'));
-    if( document.querySelector(`.${className}`) != null){
-    document.querySelector(`.${className}`).classList.remove('hidden');
+// Adding the event listener for the search form button
+searchFormButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    const data = document.querySelector('.search-form-content form');
+    const checkIn = data.querySelector('#check-in').value;  // Get the check-in value
+    const checkOut = data.querySelector('#check-out').value;  // Get the check-out value
+    const people = data.querySelector('#people').value;  // Get the number of people value
+
+    if (checkIn && checkOut && people &&
+        new Date(checkIn) <= new Date(checkOut)) {
+        reservation.startDate = checkIn;
+        reservation.endDate = checkOut;
+        reservation.guestsCount = people;
+        searchContent.style.display = 'none';
+        thankYouContent.style.display = 'block';
     }
-}
+    
+});
 
-document.querySelector('#new-reservation').addEventListener('click', (e) => cleanData(e));
-
-function cleanData(e) {
-    changeContent('search-form-content');
-}
+newReservationButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    searchContent.style.display = 'block';
+    thankYouContent.style.display = 'none';
+    // Reset the reservation object and the form fields
+    document.querySelector('#check-in').value = '';
+    document.querySelector('#check-out').value = '';
+    document.querySelector('#people').value = '';
+    
+    reservation = {
+        startDate: null,
+        endDate: null,
+        guestsCount: 0,
+        roomType: null,
+        name: null,
+        phone: null,
+        email: null
+    };
+});
